7ebc4282ffdb14561c77193a947e018785234502
Neeharika Srini <neeharika.srinivasan@gmail.com>
2024-06-06 12:16:43 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:juice_point/HomeNavPage.dart';
@@ -21,6 +23,8 @@ class MainApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.green),
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
+      // routes: 
+      
     );
   }
 }
